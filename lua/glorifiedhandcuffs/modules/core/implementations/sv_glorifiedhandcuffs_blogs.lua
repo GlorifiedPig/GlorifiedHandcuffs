48306b2179c4a6f07e3a86ca991df4566c612fc9
Ryan <ryan@glorifiedstudios.com>
2021-12-08 11:17:18 +0200

    fix bLogs implementation

--- lua/glorifiedhandcuffs/modules/core/implementations/sv_glorifiedhandcuffs_blogs.lua
+++ lua/glorifiedhandcuffs/modules/core/implementations/sv_glorifiedhandcuffs_blogs.lua
@@ -5,11 +5,11 @@ if not GlorifiedHandcuffs.Config.SUPPORT_BLOGS or not GAS or not GAS.Logging the
 local CUFFING_MODULE = GAS.Logging:MODULE()
 CUFFING_MODULE.Category = "GlorifiedHandcuffs"
 CUFFING_MODULE.Name = "Handcuffs"
-CUFFING_MODULE.Color = Color( 255, 130, 0 )
+CUFFING_MODULE.Colour = Color( 255, 130, 0 )
 
 CUFFING_MODULE:Setup( function()
     CUFFING_MODULE:Hook( "GlorifiedHandcuffs.PlayerHandcuffPlayer", "GlorifiedHandcuffs.bLogs.PlayerHandcuffPlayer", function( cuffer, cuffed )
-        MODULE:LogPhrase( "handcuffed", GAS.Logging:FormatPlayer( cuffer ), GAS.Logging:FormatPlayer( cuffed ) )
+        CUFFING_MODULE:LogPhrase( "handcuffed", GAS.Logging:FormatPlayer( cuffer ), GAS.Logging:FormatPlayer( cuffed ) )
     end )
 end )
 
@@ -20,11 +20,11 @@ GAS.Logging:AddModule( CUFFING_MODULE )
 local CUFFING_BREAKOUTS_MODULE = GAS.Logging:MODULE()
 CUFFING_BREAKOUTS_MODULE.Category = "GlorifiedHandcuffs"
 CUFFING_BREAKOUTS_MODULE.Name = "Breakouts"
-CUFFING_BREAKOUTS_MODULE.Color = Color( 255, 130, 0 )
+CUFFING_BREAKOUTS_MODULE.Colour = Color( 255, 130, 0 )
 
 CUFFING_BREAKOUTS_MODULE:Setup( function()
     CUFFING_BREAKOUTS_MODULE:Hook( "GlorifiedHandcuffs.PlayerBrokeFree", "GlorifiedHandcuffs.bLogs.PlayerBrokeFree", function( ply )
-        MODULE:LogPhrase( "handcuffs_broken", GAS.Logging:FormatPlayer( ply ) )
+        CUFFING_BREAKOUTS_MODULE:LogPhrase( "handcuffs_broken", GAS.Logging:FormatPlayer( ply ) )
     end )
 end )
 
